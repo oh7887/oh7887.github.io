462b76a31de1f60fbcedad3ea9f0a8281fb04d2c
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-02-25 17:42:54 +0800

    Create 7.sol

---
+++ 7.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Delegate {
+
+  address public owner;
+
+  constructor(address _owner) {
+    owner = _owner;
+  }
+
+  function pwn() public {
+    owner = msg.sender;
+  }
+}
+
+contract Delegation {
+
+  address public owner;
+  Delegate delegate;
+
+  constructor(address _delegateAddress) {
+    delegate = Delegate(_delegateAddress);
+    owner = msg.sender;
+  }
+
+  fallback() external {
+    (bool result,) = address(delegate).delegatecall(msg.data);
+    if (result) {
+      this;
+    }
+  }
+}
+
+
+contract math {
+    bytes4 public result;
+    function att() public  {
+        result = bytes4(keccak256("pwn()"));
+    }
+}
