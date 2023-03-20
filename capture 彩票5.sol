6efa19f7a4ac2a51e3deb731bdf6e0e05c85764b
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-03-20 15:04:15 +0800

    Update capture 彩票5.sol

--- capture 彩票5.sol
+++ capture 彩票5.sol
@@ -1,1 +1,37 @@
+pragma solidity ^0.6.0;
 
+contract PredictTheFutureChallenge {
+    address guesser;
+    uint8 guess;
+    uint256 settlementBlockNumber;
+
+    function predictTheFutureChallenge() public payable {
+        require(msg.value == 1 ether);
+    }
+
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function lockInGuess(uint8 n) public payable {
+        require(guesser == address(0));
+        require(msg.value == 1 ether);
+
+        guesser = msg.sender;
+        guess = n;
+        settlementBlockNumber = block.number + 1;
+    }
+
+    function settle() public {
+        require(msg.sender == guesser);
+        require(block.number > settlementBlockNumber);
+
+        uint8 answer = uint8(keccak256(block.blockhash(block.number - 1), now)) % 10;
+
+        guesser = address(0);
+        if (guess == answer) {
+            msg.sender.transfer(2 ether);
+        }
+    }
+}
+/* 可以看到第二十九行代码 有一个对10取模 所以 answer一共就只有 0-9 刚好十次 猜就对了*/
