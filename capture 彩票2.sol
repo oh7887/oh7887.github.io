f7e4a05541b74e76eec15b68ba1ac7a406bc7cb7
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-03-20 22:53:32 +0800

    Update capture 彩票2.sol

--- capture 彩票2.sol
+++ capture 彩票2.sol
@@ -19,7 +19,7 @@ contract GuessTheSecretNumberChallenge {
             msg.sender.transfer(2 ether);
         }
     }
-}
+}//uint8 只有255 所以直接爆破 boom~~~
 contract attack
 {
   bytes32 answerHash = 0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365;
