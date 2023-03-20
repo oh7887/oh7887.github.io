90b6362c4b63bf2a0b27e8404ff50114ceeeaaaa
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-03-20 14:07:55 +0800

    Create capture math2.sol

---
+++ capture math2.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract GuessTheSecretNumberChallenge {
+    bytes32 answerHash = 0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365;
+
+    function guessTheSecretNumberChallenge() public payable {
+        require(msg.value == 1 ether);
+    }
+    
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function guess(uint8 n) public payable {
+        require(msg.value == 1 ether);
+
+        if (keccak256(n) == answerHash) {
+            msg.sender.transfer(2 ether);
+        }
+    }
+}
+contract attack
+{
+  bytes32 answerHash = 0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365;
+  function att() public returns(uint8)
+  {
+    for(uint8 i=0;i<255;i++)
+    {
+      if(keccak256(i) == answerHash)
+      {
+        return i;
+      }
+    }
+  }
+}
