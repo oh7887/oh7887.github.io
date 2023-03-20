66bcd883f58d4762fd6bf17d5b94e63c5fe857a8
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-03-20 22:46:23 +0800

    Create capture 彩票6.sol

---
+++ capture 彩票6.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+
+contract PredictTheBlockHashChallenge {
+    address guesser;
+    bytes32 guess;
+    uint256 public settlementBlockNumber;
+
+    function predictTheBlockHashChallenge() public payable {
+        require(msg.value == 1 ether);
+    }
+
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function lockInGuess(bytes32 hash) public payable {
+        require(guesser == address(0));
+        require(msg.value == 1 ether);
+
+        guesser = msg.sender;
+        guess = hash;
+        settlementBlockNumber = block.number + 1;
+    }
+
+    function settle() public {
+        require(msg.sender == guesser);
+        require(block.number > settlementBlockNumber);
+
+        bytes32 answer = blockhash(settlementBlockNumber);
+
+        guesser = address(0);
+        if (guess == answer) {
+            msg.sender.transfer(2 ether);
+        }
+    }
+}
+/*blockhash 方法只能得到255个区块的hash 当超过255个后 blockhash方法只能返回0 */
