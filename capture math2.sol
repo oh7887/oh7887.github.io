98e7050cd5bc63694e6a2f32dccc3de121fc2253
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-03-22 11:28:23 +0800

    Create capture math2.sol

---
+++ capture math2.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.5.0;
+
+contract TokenWhaleChallenge {
+    address player;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    string public name = "Simple ERC20 Token";
+    string public symbol = "SET";
+    uint8 public decimals = 18;
+
+    function tokenWhaleChallenge(address _player) public {
+        player = _player;
+        totalSupply = 1000;
+        balanceOf[player] = 1000;
+    }
+
+    function isComplete() public view returns (bool) {
+        return balanceOf[player] >= 1000000;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function _transfer(address to, uint256 value) internal {
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;//存在溢出漏洞
+
+        emit Transfer(msg.sender, to, value);
+    }
+
+    function transfer(address to, uint256 value) public {
+        require(balanceOf[msg.sender] >= value);
+        require(balanceOf[to] + value >= balanceOf[to]);
+
+        _transfer(to, value);
+    }
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function approve(address spender, uint256 value) public {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+    }
+
+    function transferFrom(address from, address to, uint256 value) public {
+        require(balanceOf[from] >= value);
+        require(balanceOf[to] + value >= balanceOf[to]);
+        require(allowance[from][msg.sender] >= value);
+
+        allowance[from][msg.sender] -= value;
+        _transfer(to, value);//transferfrom 和transfer 函数都调用了_transfer函数
+    }
+}
+//准备三个账户 a b c
+//a账户为pleyer 并授权b账户
+//切换至b账户
+//让a向c账户转账
+//b账户发生下溢 
+//b向a账户转账
